d82fb31de95e56c626108346867c189e3de96588
bellola <luisbg9@gmail.com>
2020-02-07 16:38:30 -0500

    one more

--- easy/basic.js
+++ easy/basic.js
@@ -20,3 +20,15 @@ function largestOfFour(arr) {
   }
     return arr;
   }
+
+  // Check if a string (first argument, str) ends with the given target string (second argument, target).
+//This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge,
+// we would like you to use one of the JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+ return (str.slice(-(target.length)) === target ? true : false)
+ 
+}
