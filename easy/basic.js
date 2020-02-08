f9f7a949231fc93a47f4bb6d8766fa6c5c8f9396
bellola <luisbg9@gmail.com>
2020-02-08 15:15:43 -0500

    one more

--- easy/basic.js
+++ easy/basic.js
@@ -69,3 +69,15 @@ function truncateString(str, num) {
  }
  return result + '...'
 }
+
+//Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+//For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+function titleCase(str) {
+  let arr = str.toLowerCase().split(" ");
+  let result = arr.map(function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
