5a4b3ef4e61bc058e237c8d6ba252541ee22aba0
bellola <luisbg9@gmail.com>
2020-02-10 12:59:28 -0500

    one more

--- easy/basic.js
+++ easy/basic.js
@@ -81,3 +81,19 @@ function titleCase(str) {
   });
   return result.join(" ");
 }
+
+//Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. 
+//The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+
+  arr.sort(function(a,b){
+    return a-b
+  })
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] >= num){
+      return i
+    }
+  }
+  return arr.length
+}
