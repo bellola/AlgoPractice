7f27c73b6d355cc545d4e5540443981f17f4168a
bellola <luisbg9@gmail.com>
2020-02-10 13:50:33 -0500

    one more

--- easy/basic.js
+++ easy/basic.js
@@ -97,3 +97,16 @@ function getIndexToIns(arr, num) {
   }
   return arr.length
 }
+
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+function mutation(arr) {
+  let first = arr[0].toLowerCase()
+  let second = arr[1].toLowerCase()
+ for(let i = 0; i < second.length; i++){
+   if(first.indexOf(second[i]) < 0){
+     return false
+   }
+ }
+ return true
+}
