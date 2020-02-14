5c4576a460718af60693eb4cb8be7c1fc1021435
bellola <luisbg9@gmail.com>
2020-02-13 19:55:43 -0500

    one more

--- easy/intermidiate.js
+++ easy/intermidiate.js
@@ -10,3 +10,13 @@ function sumAll(arr) {
     }
     return result;
   }
+
+
+  //Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+  //In other words, return the symmetric difference of the two arrays.
+
+  function diffArray(arr1, arr2) {
+    return arr1
+      .concat(arr2)
+      .filter(item => !arr1.includes(item) || !arr2.includes(item));
+  }
