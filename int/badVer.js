9798c79edb0d85bbd38ee663b425fef680552053
bellola <luisbg9@gmail.com>
2020-04-04 17:41:20 -0400

    one more

--- int/badVer.js
+++ int/badVer.js
@@ -11,13 +11,13 @@ var solution = function(isBadVersion) {
      */
     return function(n) {
         // binary search
-       var start = 1, end = n;
+       var start = 1, end = n
        while(start < end){
-           var mid = Math.floor(start + (end-start) / 2);
+           var mid = Math.floor(start + (end-start) / 2)
            if(isBadVersion(mid)){
-                end = mid; // look on left side of mid
+                end = mid // look on left side of mid
            }else{
-               start = mid+1; // look on the right side of mid
+               start = mid+1 // look on the right side of mid
            }
        }
        return start;
