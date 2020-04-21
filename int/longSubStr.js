0a6a105ff111151939ca6f6563ce70b075022753
bellola <luisbg9@gmail.com>
2020-04-21 17:51:19 -0400

    new

---
+++ int/longSubStr.js
@@ -0,0 +1,22 @@
+// Given a string, find the length of the longest substring without repeating characters.
+
+var lengthOfLongestSubstring = function(s) {
+    var maxLen,
+        l,
+        r;
+        
+    if (s.length < 2) {
+        return s.length;
+    }
+    
+    maxLen = 0;
+    
+    for (l = 0, r = 1; r < s.length; r++) {
+        i = s.lastIndexOf(s[r], r-1);
+        if (i >= 0) {
+            maxLen = Math.max(maxLen, r - l);
+            l = Math.max(l, i + 1);
+        }
+    }
+    return Math.max(maxLen, r - l);
+};
