fc3c92ba38212a717757e9ba86ef05ad39e1372a
bellola <luisbg9@gmail.com>
2020-07-01 15:40:05 -0400

    one more

---
+++ easy/uniqueInOrder.js
@@ -0,0 +1,16 @@
+var uniqueInOrder=function(iterable){
+    //your code here - remember iterable can be a string or an array
+        let finalArray = [];
+      let tempArray = [];
+      if (Array.isArray(iterable) === false) {
+          tempArray = iterable.split('');
+      } else {
+          tempArray = iterable;
+      }
+      for (i = 0; i < tempArray.length; i++) {
+          if (tempArray[i] != tempArray[i - 1]) {
+              finalArray.push(tempArray[i]);
+          }
+      }
+      return finalArray;
+  }
