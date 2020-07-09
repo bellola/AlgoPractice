7ed277a427f374403d0bc563a39fb89d590dee06
bellola <luisbg9@gmail.com>
2020-07-09 19:01:26 -0400

    one more

---
+++ int/hourGlass.js
@@ -0,0 +1,15 @@
+function hourglassSum(arr) {
+    let sum = -63; 
+ for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) { 
+       let topRow = arr[row][col] + arr[row][col + 1] + arr[row][col + 2];
+       let middleRow = arr[row + 1][col + 1];
+       let bottomRow = arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2];
+       if (topRow + middleRow + bottomRow > sum) {
+         sum = topRow + middleRow + bottomRow;
+       }
+    }
+ }
+ return sum;
+
+}
