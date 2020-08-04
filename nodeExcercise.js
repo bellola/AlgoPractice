af5b99bf7d29dacf108ff74557bcab2498738d5e
bellola <luisbg9@gmail.com>
2020-08-04 12:58:03 -0400

    new

--- nodeExcercise.js
+++ nodeExcercise.js
@@ -57,6 +57,129 @@ class Node {
         this.tail = newHead
       }
     }
+
+    addToTail(data){
+        const newTail = new Node(data)
+        const currentTail = this.tail
+        if(currentTail){
+          currentTail.setNextNode(newTail);
+          newTail.setPreviousNode(currentTail)
+        }
+        this.tail = newTail
+        if(!this.head){
+          this.head = newTail
+        }
+      }
+
+      removeHead() {
+        const removedHead = this.head;
+        if (!removedHead) {
+          return;
+        }
+        this.head = removedHead.getNextNode();
+        if (this.head) {
+          this.head.setPreviousNode(null);
+        }
+        if (removedHead === this.tail) {
+          this.removeTail();
+        }
+        return removedHead.data;
+      }
+
+      removeTail(){
+        const removedTail = this.tail
+        if(!removedTail){
+          return;
+        }
+        this.tail = removedTail.getPreviousNode()
+        if(this.tail){
+          this.tail.setNextNode(null)
+        }
+        if(removedTail === this.head){
+          return this.removeHead()
+        }
+        return removedTail.data
+      }
+
+      removeByData(data) {
+        let nodeToRemove;
+        let currentNode = this.head;
+        while (currentNode !== null) {
+          if (currentNode.data === data) {
+            nodeToRemove = currentNode;
+            break;
+          }
+          currentNode = currentNode.getNextNode();
+        }
+        if (!nodeToRemove) {
+          return null;
+        }
+        // Continue your .removeByData() method below:
+        if(nodeToRemove === this.head){
+          this.removeHead()
+        }
+        else if(nodeToRemove === this.tail){
+          this.removeTail()
+        } else{
+            const nextNode = nodeToRemove.getNextNode();
+            const previousNode = nodeToRemove.getPreviousNode();
+            nextNode.setPreviousNode(previousNode);
+            previousNode.setNextNode(nextNode);
+        }
+        return nodeToRemove
+      }
+
+      //SWAPPING NODES
+      swapNodes(list, data1, data2) {
+        console.log(`Swapping ${data1} and ${data2}:`);
+        
+        let node1Prev = null;
+        let node2Prev = null;
+        let node1 = list.head;
+        let node2 = list.head;
+      
+        if (data1 === data2) {
+          console.log('Elements are the same - no swap to be made');
+          return;
+        }
+        
+        while (node1 !== null) {
+          if (node1.data === data1) { 
+            break;
+          }
+          node1Prev = node1;
+          node1 = node1.getNextNode();
+        }
+        
+        while (node2 !== null) {
+          if (node2.data === data2) {
+            break;
+          }
+          node2Prev = node2;
+          node2 = node2.getNextNode();
+        }
+        
+        if (node1 === null || node2 === null) {
+          console.log('Swap not possible - one or more element is not in the list');
+          return;
+        }
+      
+        if (node1Prev === null) {
+          list.head = node2;
+        } else {
+          node1Prev.setNextNode(node2);
+        }
+      
+        if (node2Prev === null) { 
+          list.head = node1;
+        } else {
+      node2Prev.setNextNode(node1);
+        }
+        
+        let temp = node1.getNextNode();
+        node1.setNextNode(node2.getNextNode());
+        node2.setNextNode(temp); 
+      }
     
     printList() {
       let currentNode = this.head;
