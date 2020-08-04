1b9f13cf80d91052b771469b2c9f157e90552fdd
bellola <luisbg9@gmail.com>
2020-08-04 10:42:51 -0400

    new

--- nodeExcercise.js
+++ nodeExcercise.js
@@ -1,3 +1,4 @@
+//singly linkedList
 class Node {
     constructor(data) {
       this.data = data;
@@ -34,3 +35,39 @@ class Node {
   }
   
   module.exports = Node;
+
+  //doubly linkedList
+
+  class DoublyLinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+    
+    // Create your .addToHead() method below:
+    addToHead(data){
+      const newHead = new Node(data)
+      const currentHead = this.head
+      if(currentHead){
+        currentHead.setPreviousNode(newHead);
+        newHead.setNextNode(currentHead)
+      }
+      this.head = newHead
+      if(!this.tail){
+        this.tail = newHead
+      }
+    }
+    
+    printList() {
+      let currentNode = this.head;
+      let output = '<head> ';
+      while (currentNode !== null) {
+        output += currentNode.data + ' ';
+        currentNode = currentNode.getNextNode();
+      }
+      output += '<tail>';
+      console.log(output);
+    }
+  }
+  
+  module.exports = DoublyLinkedList;
