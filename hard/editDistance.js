64e4745e1c20eb7319555c9530b7d519d4da2ceb
bellola <luisbg9@gmail.com>
2020-08-13 17:45:49 -0400

    new one

--- hard/editDistance.js
+++ hard/editDistance.js
@@ -58,4 +58,28 @@ var minDistance = function(word1, word2) {
   return count
 };
 
-//
+//recursive slow
+
+function minDistance (src, tgt) {
+    if (!tgt.length) return src.length;
+    if (!src.length) return tgt.length;
+
+    return Math.min(
+        minDistance( src.slice(1), tgt ) + 1, minDistance( tgt.slice(1), src ) + 1,
+        minDistance( src.slice(1), tgt.slice(1) ) + ( src[0] == tgt[0] ? 0 : 1 )
+    );
+}
+
+function editDistance(str1,str2, m = str1.length , n= str2.length){
+   
+    if(m===0) return n
+    if(n===0) return m
+    if(str1[m-1]== str2[n-1]) return editDistance(str1, str2, m-1, n-1) 
+    return 1 + Math.min(editDistance(str1, str2, m, n-1),    
+                   editDistance(str1, str2, m-1, n),    
+                   editDistance(str1, str2, m-1, n-1)    
+                   ) 
+
+ 
+  
+}
