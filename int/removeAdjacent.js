961467ab992815d04034dc0d39a159bca68d3d71
bellola <luisbg9@gmail.com>
2020-08-16 20:48:26 -0400

    trying cc

---
+++ int/removeAdjacent.js
@@ -0,0 +1,13 @@
+function removeAdjacent(){
+   let stack = []
+    for(let i = 0; i < S.length; i++){
+        let char = S[i]
+        let last = stack[stack.length-1]
+        if(char !== last) stack.push(char)
+        if(char === last){
+         stack.pop()
+        }
+    }
+
+    return stack.join('')
+}
