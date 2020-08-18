e9c858ff598bed2f52b54adb083670cd5f15d7e6
bellola <luisbg9@gmail.com>
2020-08-18 14:55:15 -0400

    one more

---
+++ easy/isAnagram.js
@@ -0,0 +1,26 @@
+   var isAnagram = function(s, t) {
+        if(s.length !== t.length) return false
+        if(s===t) return true
+        let hash = {};
+        for(let i in s){
+            let char = s[i]
+            if(hash[char]) hash[char]++
+            else hash[char] = 1
+        }
+       
+        
+        for(let i in t){
+            let char = t[i]
+            if(!hash[char]) return false
+            if (hash[char]) hash[char]--
+            
+        }
+        console.log(hash)
+        for(let i in hash){
+            console.log(hash[i], 'this is key')
+            if(hash[i] !== 0) return false
+            return true
+        }
+    };
+    
+    isAnagram('ab', 'a')
