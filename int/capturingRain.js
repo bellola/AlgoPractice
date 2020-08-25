9b79ff6086262fe3d86edbb668a7ced3f3f3bfdb
bellola <luisbg9@gmail.com>
2020-08-25 12:03:14 -0400

    codeacademy algos

--- int/capturingRain.js
+++ int/capturingRain.js
@@ -15,3 +15,24 @@ function naiveSolution(heights) {
     }
     return totalWater;
   }
+
+  function efficientSolution(heights) {
+    let totalWater = 0;
+    let leftPointer = 0;
+    let rightPointer = heights.length - 1;
+    let leftBound = 0;
+    let rightBound = 0;
+    
+    while (leftPointer < rightPointer) {
+      if (heights[leftPointer] <= heights[rightPointer]) {
+        leftBound = Math.max(heights[leftPointer], leftBound);
+        totalWater += leftBound - heights[leftPointer];
+        leftPointer++;
+      } else {
+        rightBound = Math.max(heights[rightPointer], rightBound);
+        totalWater += rightBound - heights[rightPointer];
+        rightPointer--;
+      }
+    }
+    return totalWater;
+  }
