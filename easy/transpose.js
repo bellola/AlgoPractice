3e67306bbb9a61fbbf03d730669ab0b238130c12
bellola <luisbg9@gmail.com>
2020-09-15 11:36:47 -0400

    now rendering prediction

--- easy/transpose.js
+++ easy/transpose.js
@@ -13,3 +13,35 @@ var transpose = function(A) {
     
     return res
 };
+
+//my sol
+
+function rotateImage(a) {
+    let temp = []
+    let res = []
+    for(let i  = 0; i<a.length;i++){
+        for(let j = 0; j < a[i].length;j++){
+            temp.push(a[j][i])
+        }
+        
+    }
+    
+    splitArray(temp, res, a)
+  
+    res.forEach(arr=>{
+        arr.reverse()
+    })
+    
+      return res
+}
+
+
+function splitArray(array, aOa, a) {
+
+    while (array.length > 0) {
+        let arrayElement = array.splice(0,a.length);
+        aOa.push(arrayElement);
+    }
+    return aOa;
+}
+
