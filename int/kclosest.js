59487ab57360603ab95f76c179c276438cd2f376
bellola <luisbg9@gmail.com>
2020-09-22 19:45:51 -0400

    new one

--- int/kclosest.js
+++ int/kclosest.js
@@ -1,8 +1,23 @@
 var findClosestElements = function(arr, k, x) {
-    if(x<arr[0]) return arr.slice(0,k)
     
-    if(x>arr[arr.length-1]) return arr.slice(-k)
+    let hash = {};
     
+    let res = [];
     
+    if(x<arr[0]) return arr.slice(0,k);
+    
+    if(x>arr[arr.length-1]) return arr.slice(-k);
+    
+    for(let i = 0; i < arr.length;i++){
+        let diff = Math.abs(x-arr[i])
+        
+        hash[arr[i]] = diff
+    }
+    
+    console.log(hash)
+    
+    for(let key of hash){
+        
+    }
     
 };
