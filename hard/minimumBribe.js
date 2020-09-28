9d4854bd0c98854ab49897b59ee8aba2aa70cbcf
bellola <luisbg9@gmail.com>
2020-09-28 19:16:34 -0400

    new one

---
+++ hard/minimumBribe.js
@@ -0,0 +1,14 @@
+function minimumBribes(q) {
+ 
+    let moves = 0;
+    for (let i = q.length - 1; i >= 0; i--) {
+        if (q[i] - (i + 1) > 2) {
+            console.log('Too chaotic')
+            return;
+        }
+        for (let j = Math.max(0, q[i] - 2); j < i; j++)
+            if (q[j] > q[i]) moves++;
+    }
+    console.log(moves)
+
+}
