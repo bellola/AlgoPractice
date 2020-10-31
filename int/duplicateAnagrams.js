b3739ffb4a25deaed5be7bc7a21070cc9e17d5c6
bellola <luisbg9@gmail.com>
2020-10-31 10:55:51 -0400

    working

--- int/duplicateAnagrams.js
+++ int/duplicateAnagrams.js
@@ -1,34 +1,42 @@
-
-
-
-function funWithAnagrams(a){
-  for(let i = 0; i< a.length;i++){
-    for(let j = a.length-1; j>i;j--){
-      if(isAnagram(a[i], a[j]) === true){
-        a.splice(j,1)
+function funWithAnagrams(a) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = a.length - 1; j > i; j--) {
+      if (isAnagram(a[i], a[j]) === true) {
+        a.splice(j, 1);
       }
     }
   }
 
-return a.sort()
+  return a.sort();
 }
 
-
-
 function isAnagram(a, b) {
-   if (a.length !== b.length) return false;
-    const map = {};
-    
-    for (let i = 0; i < b.length; i++) {
-        map[b[i]] ? map[b[i]]++ : map[b[i]] = 1;
-    }
-    
-    for (let i = 0; i < a.length; i++) {
-        if (map[a[i]]) map[a[i]]--;
-        else return false;    
-    }
+  if (a.length !== b.length) return false;
+  const map = {};
 
-    return true;
-    };
+  for (let i = 0; i < b.length; i++) {
+    map[b[i]] ? map[b[i]]++ : (map[b[i]] = 1);
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (map[a[i]]) map[a[i]]--;
+    else return false;
+  }
+
+  return true;
+}
 // isAnagram('code','doce')
-funWithAnagrams(arr)
+funWithAnagrams(arr);
+
+function moveLargest(a) {
+  let max = Math.max(...a);
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === max) {
+      a.unshift(max);
+      a.splice(i + 1, 1);
+      break;
+    }
+  }
+  return a;
+}
+
