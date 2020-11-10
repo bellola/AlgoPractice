e18efa12f190bb4bbb922ee48be1f96f36d29721
bellola <luisbg9@gmail.com>
2020-11-10 15:23:52 -0500

    new one

---
+++ int/diagonalSum.js
@@ -0,0 +1,12 @@
+var diagonalSum = function(mat) {
+    let sum = 0;
+    
+    for (let i = 0; i < mat.length; i++) {
+        sum+= mat[i][i];   
+        if (i != (mat.length - i - 1)) {
+            sum+= mat[i][mat.length - i - 1];
+        }
+    }
+    
+    return sum;
+}
