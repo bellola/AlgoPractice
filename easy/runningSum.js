70f25006714751be0706ba1e0f39b48b36199539
bellola <luisbg9@gmail.com>
2021-01-27 11:33:46 -0500

    added alt to one prob

--- easy/runningSum.js
+++ easy/runningSum.js
@@ -8,3 +8,20 @@ var runningSum = function(nums) {
  
     return nums
 };
+
+
+//worse alt
+
+var runningSum = function(nums) {
+  let arr = [nums[0]]
+  
+  for(let i = 1; i<nums.length;i++){
+    let val = 0
+    val = nums[i] += nums[i-1]
+      arr.push(val)
+      val = 0
+  }
+
+
+  return arr
+};
