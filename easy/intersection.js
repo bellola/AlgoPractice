a1bf10881ccbd5f9acf5444888d39eb3791e00c7
bellola <luisbg9@gmail.com>
2021-02-13 15:41:06 -0500

    new

--- easy/intersection.js
+++ easy/intersection.js
@@ -20,3 +20,18 @@ var intersection = function(nums1, nums2) {
        
        return res
    };
+
+   let map = new Map();
+    for(let num of nums1){
+        if(!map.has(num))
+            map.set(num, true);
+    }
+    
+     return Array.from(new Set(nums2.filter(n => {
+        if(map.has(n)){
+            return true;
+        }
+        else return false;
+    })));
+
+};
