9f56610eea7c299ca8721a74c7268962d554467b
bellola <luisbg9@gmail.com>
2021-02-14 11:54:52 -0500

    new

---
+++ int/matchingStrings.js
@@ -0,0 +1,18 @@
+function matchingStrings(strings, queries) {
+    let res = []
+    let map = {}
+    strings.forEach(s=>{
+        if(!map[s]) map[s] = 1
+        else map[s]++
+    })
+
+    queries.forEach(q=>{
+      if(map[q])res.push(map[q])
+      else res.push(0)
+    })
+    
+    return res
+    
+    
+
+}
