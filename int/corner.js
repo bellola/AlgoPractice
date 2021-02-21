a6e25fd69beb9e57177fb817d186dee99c13d664
bellola <luisbg9@gmail.com>
2021-02-21 12:45:34 -0500

    completed

--- int/corner.js
+++ int/corner.js
@@ -1,21 +1,41 @@
 let image = [
-    [0,0,0,0,0],
-    [0,0,1,1,0],
-    [0,0,1,1,0],
-    [0,0,1,1,0],
-    [0,0,0,0,0]
-  ]
-  
-  const corners = (img) =>{
-    for(let i = 0; i < img.length;i++){
-      for(let j = 0; j < img.length;j++){
-  
+  [0,0,0,0,0],
+  [0,0,1,1,0],
+  [0,0,1,1,0],
+  [0,0,1,1,0],
+  [0,0,0,0,0]
+]
+
+
+
+const corners = (img) =>{
+  let coor = []
+  let rows = []
+  let height = 0
+  let width = 0
+  for(let i = 0; i < img.length;i++){
+    if(img[i].includes(1)){
+      height+=1
+      rows.push(img[i])
+      
+    }
+    for(let j = 0; j < img.length;j++){
+      if(img[i][j]!=0){
+        coor.push([i,j])
       }
     }
-  
-    
   }
-  
-  
-  
-  corners(image)
+
+  rows[0].forEach(elem => {
+
+    if(elem===1) width++
+  })
+
+console.log(width, "WIDTH")
+console.log(height, "HEIGTH")
+console.log(coor[0], coor[coor.length-1], "BEGINNING AND END")
+}
+
+
+
+corners(image)
