5fbffbe7ea45de11891372efc43fe1cbc26ca106
bellola <luisbg9@gmail.com>
2021-02-21 12:17:43 -0500

    new o

---
+++ int/corner.js
@@ -0,0 +1,21 @@
+let image = [
+    [0,0,0,0,0],
+    [0,0,1,1,0],
+    [0,0,1,1,0],
+    [0,0,1,1,0],
+    [0,0,0,0,0]
+  ]
+  
+  const corners = (img) =>{
+    for(let i = 0; i < img.length;i++){
+      for(let j = 0; j < img.length;j++){
+  
+      }
+    }
+  
+    
+  }
+  
+  
+  
+  corners(image)
